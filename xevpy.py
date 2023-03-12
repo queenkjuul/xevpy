56d4b640f883bf588e607091de6ce74088cfc9c5
queenkjuul <queenkjuul@pm.me>
2023-03-11 19:48:54 -0600

    updatE

--- xevpy.py
+++ xevpy.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 #
+
+# This file is derived from 
 # examples/xinput.py -- demonstrate the XInput extension
-#
+# from the python-xlib library which is 
 #    Copyright (C) 2012 Outpost Embedded, LLC
 #      Forest Bond <forest.bond@rapidrollout.com>
 #
-# This library is free software; you can redistribute it and/or
+# This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License
 # as published by the Free Software Foundation; either version 2.1
 # of the License, or (at your option) any later version.
 #
-# This library is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the
+# License along with this program; if not, write to the
 #    Free Software Foundation, Inc.,
 #    59 Temple Place,
 #    Suite 330,
